d9fd669a9bf121dacd48f0d33bc525d2f0a7854f
runne <runne@128.4.153.8>
2018-10-30 08:38:11 -0400

    changes to eGameDifficulty

--- SudokuBLL/src/main/java/pkgEnum/eGameDifficulty.java
+++ SudokuBLL/src/main/java/pkgEnum/eGameDifficulty.java
@@ -4,16 +4,22 @@ import java.util.HashMap;
 
 public enum eGameDifficulty {
 	
-	EASY(100), MEDIUM(250), HARD(500);
+	EASY(100), MEDIUM(500), HARD(1000);
 	
-	private int iDifficulty;
+	private final int iDifficulty;
 	
-	private HashMap<Integer, eGameDifficulty> lookup;
+	private static final HashMap<Integer, eGameDifficulty> lookup = new HashMap<Integer, eGameDifficulty>();;
+	
+	static {
+		for (eGameDifficulty eGD : eGameDifficulty.values()) {
+			lookup.put(eGD.getDifficulty(), eGD);
+		}
+	}
 	
 	private eGameDifficulty(int iDifficulty) {
 		this.iDifficulty = iDifficulty;
 	}
-	//:)
+	
 	public int getDifficulty() {
 		return iDifficulty;
 	}
